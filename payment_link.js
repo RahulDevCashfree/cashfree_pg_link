cbf6e4f7feaff90879db851d4cd971238d439b87
RahulDevCashfree <r.rahul@cashfree.com>
2025-06-06 16:09:18 +0530

    Update payment_link.js

--- payment_link.js
+++ payment_link.js
@@ -9,12 +9,12 @@ const environment = "Test"; // Use 'Test' or 'Prod'
 const Cashfree = {
   XClientId:
     environment === "Test"
-      ? "11123595750e973ecc95c94ec5532111" // Test Client ID
-      : "145084e43c71b20eab47a2a4b80541", // Production Client ID
+      ? "{{Test_client_id}}" // Test Client ID
+      : "{{production_client_id}}", // Production Client ID
   XClientSecret:
     environment === "Test"
-      ? "TEST307e06bddd583cc3f86edf02f410fa8a69653d7d" // Test Client Secret
-      : "4fd43d83c9728bcd520f6018dea066d68cdc41d", // Production Client Secret
+      ? "{{Test_client_secret}}" // Test Client Secret
+      : "{{Production_client_secret}}", // Production Client Secret
   XEnvironment: environment,
 };
 
